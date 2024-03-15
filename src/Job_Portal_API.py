9d9a984abf476e54a07b94afc7788da0cf5ba947
Olga Aukhadieva <sarole4ka@gmail.com>
2024-03-15 17:30:43 +0500

    Add get_employers method (class HeadHunterAPI)

--- src/Job_Portal_API.py
+++ src/Job_Portal_API.py
@@ -8,7 +8,11 @@ class Job_Portal_API(ABC):
     """
 
     @abstractmethod
-    def get_vacancies(self, key_word):
+    def get_employers(self, key_word):
+        pass
+
+    @abstractmethod
+    def get_vacancies(self, emp_id):
         pass
 
 
@@ -17,20 +21,35 @@ class HeadHunterAPI(Job_Portal_API):
     Класс, наследующийся от абстрактного класса, для работы с платформой hh.ru.
     """
 
-    def get_vacancies(self, key_word: str) -> dict:
+    def get_employers(self, key_word: str) -> dict:
         """
-        Метод для подключения к API и получения вакансий.
-        Получает вакансии с hh.ru в формате JSON.
+        Метод для подключения к API и получения информации о работодателях.
+        Получает информацию с hh.ru в формате JSON.
         """
         params = {
             'text': f'{key_word}',
             'area': 113,
-            'page': 0,
+            'per_page': 10,
+            'only_with_vacancies': "true",
+        }
+
+        request = requests.get('https://api.hh.ru/employers', params)
+        employers = request.json()
+        return employers
+
+    def get_vacancies(self, emp_id: str) -> dict:
+        """
+        Метод для подключения к API и получения информации о вакансиях.
+        Получает информацию с hh.ru в формате JSON.
+        """
+        params = {
+            'area': 113,
             'per_page': 100,
+            'employer_id': f'{emp_id}',
             'enable_snippets': "true",
             'only_with_salary': "true"
         }
 
         request = requests.get('https://api.hh.ru/vacancies', params)
-        data = request.json()
-        return data
+        vacancies = request.json()
+        return vacancies
