83d166fddba3cfbaa64c6f628431d502d9629151
Olga Aukhadieva <sarole4ka@gmail.com>
2024-03-19 16:31:26 +0500

    Add exception blocks for requests

--- src/job_portal_api.py
+++ src/job_portal_api.py
@@ -33,9 +33,19 @@ class HeadHunterAPI(Job_Portal_API):
             'only_with_vacancies': "true",
         }
 
-        request = requests.get('https://api.hh.ru/employers', params)
-        employers = request.json()
-        return employers
+        try:
+            request = requests.get('https://api.hh.ru/employers', params)
+            employers = request.json()
+            return employers
+        except requests.exceptions.HTTPError as errh:
+            print('Ошибка HTTP:')
+            print(errh.args[0])
+        except requests.exceptions.ReadTimeout as errrt:
+            print("Ошибка таймаута:", errrt)
+        except requests.exceptions.ConnectionError as conerr:
+            print('Ошибка подключения:', conerr)
+        except requests.exceptions.RequestException as errex:
+            print('Ошибка запроса:', errex)
 
     def get_vacancies(self, emp_id: str) -> dict:
         """
@@ -50,6 +60,16 @@ class HeadHunterAPI(Job_Portal_API):
             'only_with_salary': "true"
         }
 
-        request = requests.get('https://api.hh.ru/vacancies', params)
-        vacancies = request.json()
-        return vacancies
+        try:
+            request = requests.get('https://api.hh.ru/vacancies', params)
+            vacancies = request.json()
+            return vacancies
+        except requests.exceptions.HTTPError as errh:
+            print('Ошибка HTTP:')
+            print(errh.args[0])
+        except requests.exceptions.ReadTimeout as errrt:
+            print("Ошибка таймаута:", errrt)
+        except requests.exceptions.ConnectionError as conerr:
+            print('Ошибка подключения:', conerr)
+        except requests.exceptions.RequestException as errex:
+            print('Ошибка запроса:', errex)
